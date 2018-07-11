7d511d70840f965f6ba631a698d7b7ad6f68a6b3
dennis <dnnsmoyo@gmail.com>
2018-07-12 00:57:40 +0200

    fixed static

--- social/settings.py
+++ social/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'social.urls'
@@ -161,4 +162,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
